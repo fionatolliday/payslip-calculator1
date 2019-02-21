aab24d78f118501355a8dc27ff9c2d5cde37af2b
Fiona Tolliday <fionatolliday@hotmail.com>
2019-02-22 10:44:40 +1100

    questions generated

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,43 @@ package com.company;
 //	create an Employee class
 //  create a PayCalculator
 
+import java.io.InputStream;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        InputStream input = System.in;
+        Scanner readInput = new Scanner(input);
+
+//        Output welcome message.
+        System.out.println("Welcome to the payslip generator!");
         //Ask for name input
+        System.out.println("Please input your name: ");
+//        get input from user
+
 //        Ask for surname input
+        System.out.println("Please input your surname: ");
+//        get input from user
+
 //        ask for annual salary
+        System.out.println("Please enter your annual salary: ");
+//        get input from user
+
 //        ask for super rate
+        System.out.println("Please enter your super rate: ");
+//        get input from user
+
 //        ask for payment start date
+        System.out.println("Please enter your payment start date: ");
+//        get input from user
+
 //        ask for payment end date
+        System.out.println("Please enter your payment end date: ");
+//        get input from user
 
 //        generate message to say "Your payslip has been generated:
+        System.out.println("Your payslip has been generated:");
 //        output name (first name + surname)
 //        output pay period (from and to)
 //        output gross income
