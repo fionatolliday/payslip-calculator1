5bb7e63a87e4aab4180ecab4554ddd6ae2591ed1
Fiona Tolliday <fionatolliday@hotmail.com>
2019-02-22 15:12:11 +1100

    added pay period and gross income

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,44 +11,41 @@ public class Main {
         InputStream input = System.in;
         Scanner readInput = new Scanner(input);
 
-//        Output welcome message.
-        System.out.println("Welcome to the payslip generator!");
-        //Ask for name input
+//        ASK QUESTIONS AND GAIN THE DATA INPUT
+        System.out.println("Welcome to the payslip generator!" + "\n");
+
         System.out.println("Please input your name: ");
-//        get input from user
         String firstName = readInput.nextLine();
-//        Ask for surname input
+
         System.out.println("Please input your surname: ");
-//        get input from user
         String surname = readInput.nextLine();
-//        ask for annual salary
+
         System.out.println("Please enter your annual salary: ");
-//        get input from user
         int annualSalary = Integer.parseInt(readInput.nextLine());
-//        ask for super rate
+
         System.out.println("Please enter your super rate: ");
-//        get input from user
         int superRate = Integer.parseInt(readInput.nextLine());
-//        ask for payment start date
+
         System.out.println("Please enter your payment start date: ");
-//        get input from user
         String startDate = readInput.nextLine();
-//        ask for payment end date
+
         System.out.println("Please enter your payment end date: ");
-//        get input from user
         String endDate = readInput.nextLine();
-        //        get input from user
-        System.out.println("Your payslip has been generated:");
 
-//        Create the template for making an instance
+
+        System.out.println("Your payslip has been generated: ");
+
+//        COMPLETE OBJECT TO USE TO PRINT TO SCREEN
         Employee employee1 = new Employee(firstName, surname, annualSalary, superRate);
+        PayCalculator pay1 = new PayCalculator(startDate, endDate, annualSalary);
 
-        //        generate message to say "Your payslip has been generated:
 
-//        output name (first name + surname)
+//      OUTPUT TO SCREEN THE GENERATED PAYSLIP DETAILS
         System.out.println("Name: " + employee1.employeeName);
 //        output pay period (from and to)
+        System.out.println("Pay Period: " + pay1.payPeriod(startDate, endDate));
 //        output gross income
+        System.out.print("Gross Income: " + pay1.grossIncome(annualSalary));
 //        output income tax
 //        output net income
 //        output super
--- src/com/company/PayCalculator.java
+++ src/com/company/PayCalculator.java
@@ -3,16 +3,28 @@ package com.company;
 public class PayCalculator {
 
 //    Declare the variables
+    public final String employeePayPeriod;
+    public final int employeeGrossIncome;
 
 
 
 //    Define a method to calculate pay period
-
+    public String payPeriod(String startDate, String endDate){
+        return startDate + "-" + endDate;
+    }
 //    Define a method to calculate gross income
+    public int grossIncome(int annualSalary){
+        return annualSalary/12;
+    }
 //    Define a method to calculate income tax
+    
 //    Define a method to calculate net income
 //    Define a method to calculate super
 
 
 //    construct pay instance/object
+    public PayCalculator (String startDate, String endDate, int annualSalary){
+        this.employeePayPeriod = payPeriod(startDate, endDate);
+        this.employeeGrossIncome = grossIncome(annualSalary);
+    }
 }
